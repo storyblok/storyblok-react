bbc2503f83f7a2af1deb39de5dfc2f444983efd8
Alexander Feiglstorfer <delooks@gmail.com>
2016-10-29 22:29:34 +0200

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Storyblok Editable Component
+
+Wrap this component to make your components editable in [storyblok.com](https://www.storyblok.com/)
+
+###Installation
+    npm install storyblock-editable --save-dev
+
+    // ES6
+    import SbEditable from 'storyblock-editable'
+
+
+###Example
+
+    <SbEditable content={this.props.content}>
+      <p>
+        {this.props.content.text}
+      </p>
+    </SbEditable>
+
+
+##License
+MIT
---
+++ src/sb-editable.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class SbEditable extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    if (typeof this.props.content._editable === 'undefined') {
+      return
+    }
+
+    var el = ReactDOM.findDOMNode(this)
+    var options = JSON.parse(this.props.content._editable.replace('<!--#storyblok#', '').replace('-->', ''))
+
+    el.setAttribute('data-blok-c', JSON.stringify(options))
+    el.setAttribute('data-blok-uid', options.id + '-' + options.uid)
+
+    this.addClass(el, 'storyblok__outline')
+  }
+
+  addClass(el, className) {
+    if (el.classList) {
+      el.classList.add(className)
+    } else if (!hasClass(el, className)) {
+      el.className += ' ' + className
+    }
+  }
+
+  render() {
+    return this.props.children
+  }
+}
+
+export default SbEditable
