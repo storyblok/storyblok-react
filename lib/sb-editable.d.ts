d330fda23b1dd1f8659402ea0149a1cc851b8451
John Lombardo <john@programbo.com>
2018-07-15 08:43:40 +0800

    Extract content from props

--- lib/sb-editable.d.ts
+++ lib/sb-editable.d.ts
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
+export interface SbEditableContent {
+  _uid: string
+  _editable?: string
+  component: string
+  [index: string]: any
+}
 interface SbEditableProps {
-    content: {
-        _editable?: string;
-    };
+  content: SbEditableContent
 }
 declare class SbEditable extends React.Component<SbEditableProps, {}> {
-    constructor(props: SbEditableProps);
-    componentDidMount(): void;
-    addClass(el: HTMLElement, className: string): void;
-    render(): React.ReactNode;
+  constructor(props: SbEditableProps)
+  componentDidMount(): void
+  addClass(el: HTMLElement, className: string): void
+  render(): React.ReactNode
 }
-export default SbEditable;
+export default SbEditable
