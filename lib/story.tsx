b83e8613689b437ccd1617fc3dc5cc61a29f7135
Chakit Arora <arorachakit@gmail.com>
2023-07-14 17:35:36 +0530

    add bridge options to StoryblokStory

--- lib/story.tsx
+++ lib/story.tsx
@@ -3,19 +3,20 @@
 import React, { forwardRef } from "react";
 import { useStoryblokState } from "./common/client";
 import StoryblokComponent from "./common/storyblok-component";
-import { ISbStoryData } from "./types";
+import { ISbStoryData, StoryblokBridgeConfigV2 } from "./types";
 
 interface StoryblokStoryProps {
   story: ISbStoryData;
+  bridgeOptions: StoryblokBridgeConfigV2;
   [key: string]: unknown;
 }
 
 const StoryblokStory = forwardRef<HTMLElement, StoryblokStoryProps>(
-  ({ story, ...restProps }, ref) => {
+  ({ story, bridgeOptions, ...restProps }, ref) => {
     if (typeof story.content === "string") {
       story.content = JSON.parse(story.content);
     }
-    story = useStoryblokState(story);
+    story = useStoryblokState(story, bridgeOptions);
     return <StoryblokComponent ref={ref} blok={story.content} {...restProps} />;
   }
 );
