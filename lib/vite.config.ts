eb2261805df3daec1808dbb9f1d6cd4196bc701c
Alex Jover <alexjovermorales@gmail.com>
2023-07-28 13:36:16 +0200

    feat: enable single-bundle import for RSC

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,35 +1,28 @@
 import { defineConfig } from "vite";
 import path from "path";
+import preserveDirectives from "rollup-plugin-preserve-directives";
 
 const libName = "storyblok-react";
 
 export default defineConfig({
+  plugins: [preserveDirectives()],
   build: {
     lib: {
       entry: [
         path.resolve(__dirname, "index.ts"),
         path.resolve(__dirname, "rsc/index.ts"),
-        path.resolve(__dirname, "bridge-loader.tsx"),
-        path.resolve(__dirname, "story.tsx"),
       ],
       name: "storyblokReact",
-      fileName: (format) =>
-        format === "es" ? `${libName}.mjs` : `${libName}.js`,
-    },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        directives: false,
+      fileName: (format, entryName) => {
+        const name = entryName.split("/").pop();
+        return format === "es" ? `${name}.mjs` : `${name}.js`;
       },
     },
     rollupOptions: {
       external: ["react"],
       output: {
+        preserveModules: true,
         globals: { react: "React" },
-        banner: (chunk) =>
-          ["bridge-loader", "story"].includes(chunk.name)
-            ? '"use client";'
-            : "",
       },
     },
   },
