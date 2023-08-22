d9318c932c0093de83ab8053da1ff8a56ac32ec2
Facundo Giuliani <facundo.giuliani@gmail.com>
2023-08-22 14:21:30 -0300

    fix: Add alias

--- lib/rsc/index.ts
+++ lib/rsc/index.ts
@@ -1,3 +1,4 @@
 export * from "../common";
 export { default as StoryblokStory } from "../story";
+export { default as BridgeLoader } from "../bridge-loader";
 export { default as StoryblokBridgeLoader } from "../bridge-loader";
