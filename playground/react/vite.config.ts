9cff8731fabba0bb0fbb20b21d8e9482d327b31e
alvarosabu <alvaro.saburido@gmail.com>
2024-12-10 16:06:25 +0100

    chore: isolate ci and update deps

--- playground/react/vite.config.ts
+++ playground/react/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@storyblok/react': resolve(__dirname, '../../src/index.ts'),
+    },
+  },
 });
