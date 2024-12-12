6e3a4bf2ab156531308b120fdb056ba39e587fad
alvarosabu <alvaro.saburido@gmail.com>
2024-12-12 15:52:06 +0100

    chore: added tests to component resolver

--- src/richtext.ts
+++ src/richtext.ts
@@ -3,11 +3,10 @@ import StoryblokComponent from './common/storyblok-component';
 import type { StoryblokRichTextNode, StoryblokRichTextOptions } from '@storyblok/js';
 import { BlockTypes, richTextResolver } from '@storyblok/js';
 
-function componentResolver(node: StoryblokRichTextNode<React.ReactElement>) {
-  // Convert this to use React.createElement or JSX
-  // Example with JSX:
+export function componentResolver(node: StoryblokRichTextNode<React.ReactElement>): React.ReactElement {
+  const body = node?.attrs?.body;
   return React.createElement(StoryblokComponent, {
-    blok: node?.attrs?.body[0],
+    blok: Array.isArray(body) ? body[0] : undefined,
     key: node.attrs?.id,
   });
 }
